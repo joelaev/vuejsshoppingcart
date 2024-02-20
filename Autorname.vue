8d823fe3fa7f7d134f493b632f9fb004435e08dc
Joel Escobar <escobarvj117@gmail.com>
2024-02-20 12:49:08 -0600

    👌feat (project) add boilerplate se agregan nuevos achivos de proyecto inicial

---
+++ Autorname.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="div">
+    <h1>autor name</h1>
+    <p>
+        <a target="_blank" href="https://github.com/joelaev">
+            Joel Escobar
+        </a>
+    </p>
+    </div>
+    </template>
