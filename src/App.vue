6fb61e06323977a492882169fc7bedb43bd87bec
Joelaev <escobarvj117@gmail.com>
2024-03-01 11:51:31 -0600

    ⭐Feat: list reading Add a list of item using v ffor

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,22 @@ import {ref} from 'vue'
 //crendo referencia constante activa (conjunto de variables es el "estado de componente")
 const header =ref('app lista de compras')
 const shoppingIcons =ref('material-icons shopping-cart-icon');
+//creando una referencia para
+  //almacenar elvalor de una lista
+  const items = ref([
+    {id: 1, label:'Leche'},
+   { id: 1, label:'Arroz'}
+    {id: 1, label:'Carne'},
+   {id: 1, label:'Pan'},
+   {id: 1, label:'huevos'}
+  ])
 </script>
 
 <template>
   <h1><i :class="shoppingIcons">local_ mall</i> {{ header }}</h1>
+  <ul>
+    <li v-for="item in items" divvi b-bind: key="item">⭐ {(item.label)}</li>
+  </ul>
 </template>
 
 <style scoped>
