f37c4593a5b3024f2b01bcc4ae2701c68df50257
Joelaev <escobarvj117@gmail.com>
2024-03-10 21:15:39 -0600

    ⭐Feat appvue add list

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,22 @@ import {ref} from 'vue'
 //crendo referencia constante activa (conjunto de variables es el "estado de componente")
 const header =ref('app lista de compras')
 const shoppingIcons =ref('material-icons shopping-cart-icon');
+//creando una referencia para
+  //almacenar elvalor de una lista
+  const items = ref([
+   {id: 0, label:'Leche'},
+   {id: 1, label:'Arroz'},
+   {id: 2, label:'Carne'},
+   {id: 3, label:'Pan'},
+   {id: 4, label:'huevos'}
+  ])
 </script>
 
 <template>
-  <h1><i v-bind:class="shoppingIcons">local_mall</i> {{ header }}</h1>
+  <h1><i :class="shoppingIcons">local_mall</i> {{ header }}</h1>
+  <ul>
+    <li v-for="item in items" v-bind:key="item.id">⭐ {{item.label}}</li>
+  </ul>
 </template>
 
 <style scoped>
