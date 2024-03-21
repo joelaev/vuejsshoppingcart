36487cfe9b207532e2b9382f6a3278b917c76a58
Joelaev <escobarvj117@gmail.com>
2024-03-21 12:08:48 -0600

    ⭐Feat: purchased

--- src/App.vue
+++ src/App.vue
@@ -7,43 +7,43 @@ const shoppingIcons =ref('material-icons shopping-cart-icon');
 //creando una referencia para
   //almacenar elvalor de una lista
   const items = ref([
-   //{id: 0, label:'Leche'},
-   //{id: 1, label:'Arroz'},
-   //{id: 2, label:'Carne'},
-   //{id: 3, label:'Pan'},
-   //{id: 4, label:'huevos'}
-  ])
+   {id: 0, label:'Leche', purchased:false, highPriority: true},
+   {id: 1, label:'Arroz', purchased:false, highPriority: false},
+   {id: 2, label:'Carne', purchased:true, highPriority: true},
+   {id: 3, label:'Pan', purchased:false, highPriority: false},
+   {id: 4, label:'huevos', purchased:true, highPriority: true}
+  ]);
+  const togglePurchased=(item)=>{
+    item.purchased=!item.purchased
+  }; 
   const newItem =ref('')
-  const newItemHighPriority =ref(false)
+  const newItemHighPriority =ref('false')
   //metodos
   const saveItems=()=>{
     //agrega un nuevo item provenuente de la caja de texto
     items.value.push({id: items.value.length, label:newItem.value})
     //borrar contenido de la caja de texto
     newItem.value="";
+  }  
+  const doEdit=(edit)=>{
+    editing.value=edit
+    newItem.value="";
   }
-  const add=ref(true)
-  const toggleForm=()=>{
-    add.value=!add.value
-  }
+  const editing =ref(false)
 </script>
+
 <template>
   <!-- Header -->
   <div class="header">
     <h1><i :class="shoppingIcons">local_mall</i> {{ header }}
     </h1>
-    
-    <!-- Botón "Agregar Articulo" -->
-    <p v-if="add">
-    <button class="btn btn-primary" @click="toggleForm">Agregar Articulo</button>
-    </p>
-    <!-- Botón "Cancelar" -->
-    <p v-if="!add"><button class="btn" @click="toggleForm">Cancelar</button></p>
-  </div> 
+    <button  v-on:click="doEdit(false)" v-if="editing" 
+    class="btn">Cancelar</button>
+    <button v-else v-on:click="doEdit(true)"
+     class="btn btn-primary">Agregar Articulo</button>
+  </div>  
 
-  <!-- Formulario para agregar un item -->
-  <p v-if="!add"> 
-  <form
+  <form v-if="editing"
   v-on:submit.prevent="saveItems"
    class="add-item form">
   <input v-model="newItem" type="text" placeholder ="agregar articulo">
@@ -52,19 +52,23 @@ const shoppingIcons =ref('material-icons shopping-cart-icon');
   </label>
   <button class="btn btn-primary">Agregar Articulo</button>
   </form>
-</p>
-
-  <!-- Lista de items -->
-  <ul>
-    <li v-for="item in items" v-bind:key="item.id">⭐ {{ item.label }}</li>
+  <!-- entrega de lista -->
+    <ul>
+    <li v-for="({id, label,purchased, highPriority}, index) in items" 
+    @click="togglePurchased(items[index])"
+    :class="{priority:highPriority, strikeout:purchased}"
+    v-bind:key="id">⭐ {{label}}</li>
   </ul>
-  <!-- Mensaje condicional -->
+    <!-- <ul>
+    <li v-for="{id, label, purchased, highPriority} in items" 
+    :class="['purchased' ?  'strikeout' : '', hightPriority ? 'priority': '']"
+    v-bind:key="id">⭐ {{label}}</li>
+  </ul> -->
+  <!-- mensaje condicional -->
   <p v-if="items.length==0">🥀No hay elementos en la lista</p>
 </template>
-
 <style scoped>
 .shopping-cart-icon{
 font-size: 2 rm;
 }
 </style>
-
